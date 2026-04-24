1b441478d4ce2864113b6046c7e9e344c2995224
eliuthlarios <00029223@uca.edu.sv>
2026-04-24 10:31:18 -0600

    :white_check_mark: creando proyecto

---
+++ NcLabo1/src/main/java/com/uca/nclabo1/NcLabo1Application.java
@@ -0,0 +1,13 @@
+package com.uca.nclabo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcLabo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcLabo1Application.class, args);
+    }
+
+}
---
+++ NcLabo1/src/test/java/com/uca/nclabo1/NcLabo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.nclabo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcLabo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
